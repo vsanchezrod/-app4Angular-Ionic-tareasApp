71728dc59789931533c2bd8f2b3d0e6d03ba70f4
vsanchezrod <v.sanchez.rod@gmail.com>
2018-07-24 09:21:31 +0200

    Usando AlertController y NavParams para crear lista nueva y añadir tareas

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { TareasService } from '../../services/tareas.services';
+import { Lista, ListaItem } from '../../models';
+
+// En IONIC NavParams se utiliza para recuperar la información que se le envía. En este caso desde la página de pendientes.
+import { NavParams } from "ionic-angular";
 
 @Component ({
     selector: 'page-agregar',
@@ -9,8 +13,38 @@ import { TareasService } from '../../services/tareas.services';
 
 export class AgregarPage {
 
-    constructor (public tareasService: TareasService) {
+    lista: Lista;
+    nombreItem: string = '';
 
+    constructor (public tareasService: TareasService,
+                 private navParams: NavParams) {
+
+        // Para ver que información que llega
+        // Se podría poner this.navParams y tendríamos el objeto entero o se puede utilizar el método get y pasarle la propiedad que se quiere obtener
+        const titulo = this.navParams.get('titulo');
+
+        // El constructor de la lista solo necesitaba el título
+        this.lista = new Lista (titulo);
     }
 
+    agregarItem() {
+        // Validación de que no este vacío
+        if (this.nombreItem.length === 0) {
+            return;
+        }
+        
+        // El modelo ListaItem, solo necesita una descripción en su constructor que será lo que introduzca en el input
+        const nuevoItem = new ListaItem(this.nombreItem);
+    
+        // Se añade el nuevoItem a la lista
+        this.lista.items.push(nuevoItem);
+
+        // Se borra el valor del input para que el usuario pueda introducir más items
+        this.nombreItem = '';
+    }
+
+    actualizarEstado(item: ListaItem) {
+        // Cuando se haga click se cambiará el estado de completado o no en función del valor actual
+        item.completado = !item.completado;
+    }
 }
--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -4,10 +4,14 @@ import { Component } from '@angular/core';
 // Se importa el servicio para poder usarlo y se pasa al contructor
 import { TareasService } from '../../services/tareas.services';
 import { Lista } from '../../models/lista.model';
-
-// En IONIC NacController nos permite navegar
-import { NavController } from 'ionic-angular';
 import { AgregarPage } from '../agregar/agregar.component';
+
+// En IONIC NacController nos permite navegar a otra página
+// En IONIC AlertController permite crear alertas o pop-ups
+import { NavController, AlertController } from 'ionic-angular';
+
+
+
 
 @Component ({
     selector: 'page-pendientes',
@@ -17,7 +21,8 @@ import { AgregarPage } from '../agregar/agregar.component';
 export class PendientesPage {
 
     constructor(public tareasService: TareasService,
-                private navCtrl: NavController) {
+                private navCtrl: NavController,
+                private alertCtrl: AlertController) {
 
     }
 
@@ -27,8 +32,38 @@ export class PendientesPage {
     }
 
     agregarLista() {
-        // Se utiliza el método push para agregar una página al stack de páginas
-        this.navCtrl.push(AgregarPage);
+                
+        // Se crea una alerta con el objeto alertCtrl y el método create y se manda en el método un objeto de configuración
+        const alerta = this.alertCtrl.create({
+            title: 'Nueva lista',
+            message: 'Nombre de la nueva lista',
+            inputs: [{
+                name: 'titulo',
+                placeholder: 'Nombre de la lista'
+            }],
+            buttons: [{
+                text: 'Cancelar'
+            }, {
+                text: 'Agregar',
+                // Este botón tendrá un manejador que hará una validación
+                handler: data => {
+                    if (data.titulo.length === 0) {
+                        return;
+                    }
+                    
+                    // Se utiliza el método push para cambiar a la otra página, y se le pasa entre {} el objeto q se le quiere mandar
+                    this.navCtrl.push(AgregarPage, {
+                        titulo: data.titulo
+                    });
+                    
+                    }
+                }
+            ]
+        });
+
+        // Para presentar la alerta
+        alerta.present();
+
     }
 
 }
